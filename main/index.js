14c6d2ba36f24ccd75074788ae87594f35b38820
khimxzxc <127808298+khimxzxc@users.noreply.github.com>
2024-07-19 17:14:49 +0500

    Add files via upload

---
+++ main/index.js
@@ -0,0 +1,15 @@
+document.getElementById("popup").addEventListener("click", function() {
+    document.getElementById("menu").classList.add("ok")
+})
+
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("menu").classList.remove("ok")
+    }
+});
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById("menu").classList.remove("ok")
+})
